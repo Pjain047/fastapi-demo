4753504dac3504635e0c8c33ddce54127fbff962
Pjain047 <prashantjain047@gmail.com>
2026-09-07 20:12:02 -0500

    Add k8s files for minikube local

--- app/config.py
+++ app/config.py
@@ -8,13 +8,16 @@ class Settings(BaseSettings):
     app_name: str = "FastAPI Demo With Task Management API"
     app_description: str = "A FastAPI Demo with Task Management API to learn DevOps"
     app_version: str = "1.0.0"
-    environment: Literal["local", "development", "test", "production"] = "local"
+    environment: Literal["local", "development", "test", "production","minikube"] = "local"
     debug: bool = False
 
     host: str = "0.0.0.0"
     port: int = 8000
 
     log_level: Literal["debug", "info", "warning", "error", "critical"] = "info"
+
+    demo_api_key: str | None = None
+    demo_username: str | None = None
 
     model_config = SettingsConfigDict(
         env_file=".env",
